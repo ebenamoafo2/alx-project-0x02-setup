ad75177884d36220512a6e3598fc1a2d1501ae3f
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-26 11:25:49 +0000

    Renamed project directory to match task requirements

--- alx-project-2/components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { HeaderProps } from '@/interfaces'
-
-export default function Header ({ title }: HeaderProps){
-    return(
-        <header className="bg-blue-600 text-white shadow-md text-center">
-            {title}
-        </header>
-    )
-}
--- alx-project-2/interfaces/index.ts
+++
@@ -1,9 +0,0 @@
-
-export interface NavLink {
-  label: string;
-  href: string;
-}
-
-export interface HeaderProps {
-  title: string;
-}
